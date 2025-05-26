0becf4153b172d0bdc7b475aabda3e82b029247a
hemant5037 <hemantkumaryadav542@gmail.com>
2025-05-26 15:48:13 +0530

    routes defined

---
+++ backend/routes/appointmentRouter.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  deleteAppointment,
+  getAllAppointments,
+  postAppointment,
+  updateAppointmentStatus,
+} from "../controller/appointmentController.js";
+import {
+  isAdminAuthenticated,
+  isPatientAuthenticated,
+} from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.post("/post", isPatientAuthenticated, postAppointment);
+router.get("/getall", isAdminAuthenticated, getAllAppointments);
+router.put("/update/:id", isAdminAuthenticated, updateAppointmentStatus);//:id because we want the id
+router.delete("/delete/:id", isAdminAuthenticated, deleteAppointment);
+
+export default router;
