130b422409e77e2a257b6eb1838a2174f28a540f
hemant5037 <hemantkumaryadav542@gmail.com>
2025-06-12 09:39:23 +0530

    completed almost my dashboard part also

---
+++ dashboard/src/components/Messages.jsx
@@ -0,0 +1,65 @@
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { Context } from "../main";   //comes from main file
+import { Navigate } from "react-router-dom";
+
+const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const { isAuthenticated } = useContext(Context);
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4000/api/v1/message/getall",
+          { withCredentials: true }
+        );
+        setMessages(data.messages); //that messages come from backend
+      } catch (error) {
+        console.log(error.response.data.message);
+      }
+    };
+    fetchMessages();
+  }, []);
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/login"} />;
+  }
+
+  return (
+    <section className="page messages">
+      <h1>MESSAGE</h1>
+      <div className="banner">
+        {messages && messages.length > 0 ? ( //if messages exist and length of messages greater than 0
+          messages.map((element) => {
+            return (
+              <div className="card" key={element._id}>
+                <div className="details">
+                  <p>
+                    First Name: <span>{element.firstName}</span>
+                  </p>
+                  <p>
+                    Last Name: <span>{element.lastName}</span>
+                  </p>
+                  <p>
+                    Email: <span>{element.email}</span>
+                  </p>
+                  <p>
+                    Phone: <span>{element.phone}</span>
+                  </p>
+                  <p>
+                    Message: <span>{element.message}</span>
+                  </p>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <h1>No Messages!</h1>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Messages;
