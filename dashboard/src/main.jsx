581d843bbecdde598db01dafa96867c270eded7e
hemant5037 <hemantkumaryadav542@gmail.com>
2025-06-12 22:08:12 +0530

    some changes to dashboard part

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,12 +1,30 @@
 import React, { createContext, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import axios from "axios";
 
 export const Context = createContext({ isAuthenticated: false });
 
 const AppWrapper = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [admin, setAdmin] = useState({});
+
+  React.useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://hostelmanagement-v7e3.onrender.com/api/v1/user/admin/me",
+          { withCredentials: true }
+        );
+        setIsAuthenticated(true);
+        setAdmin(data.admin);
+      } catch {
+        setIsAuthenticated(false);
+        setAdmin(null);
+      }
+    };
+    checkAuth();
+  }, []);
 
   return (
     <Context.Provider
