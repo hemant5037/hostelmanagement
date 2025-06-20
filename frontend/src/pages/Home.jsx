440f4ea66c4c6cbb45e32276006a817f67793503
hemant5037 <hemantkumaryadav542@gmail.com>
2025-06-20 22:38:55 +0530

    c

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         title={
           "Welcome to Nivora Care Hospital –  Your Partner in Health and Healing"
         }
-        imageUrl={"/dispensary.png"}
+        imageUrl={"/hero.jpg"}
       />
       <Biography imageUrl={"/aboutus.jpg"} />
       <Departments />
